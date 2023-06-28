f2f83834ae081123b48f21ae5bfb5e2c43be162d
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-28 16:53:32 -0300

    importando a página Player

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./store";
+import { Player } from "./pages/Player";
+import "./styles/global.css";
+
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <ReduxProvider store={store}>
+      <Player />
+    </ReduxProvider>
+  );
 }
